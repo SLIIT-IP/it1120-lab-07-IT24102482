1b5d4a44cc1ea1cbb4648f5b0c5ef0d80abd7268
IT24102482 <it24102482@my.sliit.lk>
2024-09-24 21:33:03 +0530

    Add IT24102482Lab7Q2 to Q2

---
+++ Q2/IT24102482Lab7Q2A.java
@@ -0,0 +1,11 @@
+public class IT24102482Lab7Q2A{
+	public static void main(String[] args){
+
+	for(int j=1; j<=4; j++){
+	for(int i=1; i<=5; i++){
+		System.out.println("$");
+	}
+		System.out.println();
+	}
+}
+}
