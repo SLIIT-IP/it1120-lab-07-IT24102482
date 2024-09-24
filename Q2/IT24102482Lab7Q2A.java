b4a2bf5060c4456437d95b4792e64f486ef3e375
IT24102482 <it24102482@my.sliit.lk>
2024-09-24 21:39:54 +0530

    Add IT24102482Lab7Q2A.java (Recorrect) to Q2

--- Q2/IT24102482Lab7Q2A.java
+++ Q2/IT24102482Lab7Q2A.java
@@ -1,11 +1,10 @@
 public class IT24102482Lab7Q2A{
-	public static void main(String[] args){
-
-	for(int j=1; j<=4; j++){
-	for(int i=1; i<=5; i++){
-		System.out.println("$");
-	}
-		System.out.println();
+public static void main(String[] args){
+	for(int i=1; i<=4; i++){
+		for(int j=1; j<=5; j++){
+		System.out.print("$");
+		}
+	System.out.println();
 	}
 }
 }
