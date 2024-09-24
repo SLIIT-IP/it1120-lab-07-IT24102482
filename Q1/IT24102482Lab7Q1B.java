576ce66920eecc01047f36602ade95aa24c05214
IT24102482 <it24102482@my.sliit.lk>
2024-09-24 21:31:52 +0530

    Add IT24102482Lab7Q1A.java to Q1

---
+++ Q1/IT24102482Lab7Q1B.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class IT24102482Lab7Q1B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	
+        for(int student = 1; student <= 3; student++) {
+            System.out.println("student " + student);
+	
+	System.out.println("Enter Subject Mark 1: ");
+	int mark_1 = sc.nextInt();
+
+	System.out.println("Enter Subject Mark 2: ");
+	int mark_2 = sc.nextInt();
+
+	System.out.println("Enter Subject Mark 3: ");
+	int mark_3 = sc.nextInt();
+
+	System.out.println("Enter Subject Mark 4: ");
+	int mark_4 = sc.nextInt();
+
+	System.out.println("Enter marks: "+mark_1+ " " +mark_2+ " " +mark_3+ " " +mark_4);
+
+	int avg = (mark_1+mark_2+mark_3+mark_4)/4;
+	System.out.println("Average is: "+avg);
+
+	if(avg>=75 && avg<=100)
+	{
+		System.out.println("Overal Grade is Distinction");
+	}
+	else if(avg>=50 && avg<75)
+	{
+		System.out.println("Overal Grade is Credit");
+	}
+	else{
+		
+		System.out.println("Overal Grade is Fail");
+	}
+	}
+}
+}
